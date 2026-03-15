a61ea93224da6d32c2cbec84e338269a12749ade
Sonam Lindel <Add_Your_Email_Here>
2026-03-15 15:51:52 +0600

    p

---
+++ my-app/src/app/page.js
@@ -0,0 +1,9 @@
+import VideoFeed from "../components/ui/VideoFeed";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <VideoFeed />
+    </div>
+  );
+}
